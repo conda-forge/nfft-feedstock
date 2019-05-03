fbf5b61b1c1f21a390b7505f5e80b027f600d1fe
Holger Kohr <ho.kohr@zoho.com>
2019-05-04 01:22:01 +0200

    Build library for single, double and extended precison

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,3 +27,31 @@ fi
 make
 make check
 make install
+
+make clean
+./configure --prefix=$PREFIX \
+            --enable-applications \
+            --enable-all \
+            --enable-openmp \
+            --enable-single \
+            --with-fftw3-libdir=$PREFIX/lib \
+            --with-fftw3-includedir=$PREFIX/include \
+            --with-window=kaiserbessel
+
+make
+make check
+make install
+
+make clean
+./configure --prefix=$PREFIX \
+            --enable-applications \
+            --enable-all \
+            --enable-openmp \
+            --enable-long-double \
+            --with-fftw3-libdir=$PREFIX/lib \
+            --with-fftw3-includedir=$PREFIX/include \
+            --with-window=kaiserbessel
+
+make
+make check
+make install
